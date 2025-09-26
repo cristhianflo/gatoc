4980743b04d0c831e0dda7a7f30c40e01afda62b
bachacode <85910843+bachacode@users.noreply.github.com>
2025-09-25 22:11:45 -0400

    feat: created new dolar command with subcommand "estado" to check api status

---
+++ internal/commands/dolar.go
@@ -0,0 +1,34 @@
+package commands
+
+import (
+	"fmt"
+
+	"github.com/bachacode/gatoc/internal/bot"
+	subcommands "github.com/bachacode/gatoc/internal/commands/dolar"
+	"github.com/bwmarrin/discordgo"
+)
+
+func init() {
+	bot.RegisterCommand(dolar.Metadata.Name, dolar)
+}
+
+var dolar bot.SlashCommand = bot.SlashCommand{
+	Metadata: &discordgo.ApplicationCommand{
+		Name:        "dolar",
+		Description: "Cotización del dolar a bolívares",
+		Options: []*discordgo.ApplicationCommandOption{
+			subcommands.Estado.Metadata,
+		},
+	},
+	Handler: func(s *discordgo.Session, i *discordgo.InteractionCreate, ctx *bot.BotContext) error {
+		options := i.ApplicationCommandData().Options
+
+		switch options[0].Name {
+		case "estado":
+			return subcommands.Estado.Handler(s, i, ctx)
+		default:
+			bot.GetInteractionFailedResponse(s, i, "El subcomando llamado no existe.")
+			return fmt.Errorf("Subcommand doesn't exist\n")
+		}
+	},
+}
